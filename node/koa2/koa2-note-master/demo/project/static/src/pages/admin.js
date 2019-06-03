48f6e2162c0a8cba0d2f57bb431a4b3f01f87ab2
xyd <yingdong_xiao@sfabric.com>
2019-06-03 10:19:05 +0800

    'blog'

--- node/koa2/koa2-note-master/demo/project/static/src/pages/admin.js
+++
@@ -1,8 +0,0 @@
-import '@babel/polyfill';
-import 'whatwg-fetch'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './../apps/admin.jsx'
-
-ReactDOM.render( <App />,
-  document.getElementById("app"))
